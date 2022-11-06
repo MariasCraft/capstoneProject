e7f736995f0f3c796d52098475630c3721a75305
asoikaw15 <krteammain@gmail.com>
2022-11-06 23:54:28 +0800

    add database connection

--- server.js
+++ server.js
@@ -8,42 +8,55 @@ import session from "express-session"
 import passport from "passport"
 import localStrategy from "passport-local"
 import MongoStore from "connect-mongo"
+import User from './models/Users.js'
 
-dotenv.config ()     //to use the process dotenv variable
+
+dotenv.config()     //to use the process dotenv variable
 
 //initialization of app
-const app = express () 
-const __dirname = path.dirname (fileURLToPath (import.meta.url)) 
+const app = express()
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 //mongoes are open database connection
-mongoose.connect (process.env.database)
-mongoose.connection.on ("error", console.error.bind (console, "Connection Error"))
-mongoose.connection.once ("open", () => {
-          console.log ("connected")
+mongoose.connect(process.env.database)
+mongoose.connection.on("error", console.error.bind(console, "Connection Error"))
+mongoose.connection.once("open", () => {
+    console.log("connected")
 })
-const store = new MongoStore ({
-          mongoUrl: process.env.database,
-          touchAfter: 24*60*60
+const store = new MongoStore({
+    mongoUrl: process.env.database,
+    touchAfter: 24 * 60 * 60
+})
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/add', async (req, res) => {
+    const newUser = {
+        username: req.body.user,
+        password: req.body.pass,
+        name: req.body.name,
+    }
+    await User.create(newUser)
+    res.send('sent')
+})
+
+app.get('/add', (req, res) => {
+    res.render('add')
 })
 
 
-// below are for the routes syntax
-
-app.get("/", (req,res) =>  {
-         res.render ("index")
+app.get("/", (req, res) => {
+    res.render("index")
 })
 
-app.get("/home", (req,res) =>  {
-          res.render ("home")
- })
 
- 
-app.set ("view engine", "ejs")
-app.set ("views", path.join(__dirname,"views"))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
 
+app.static
 
 
-
-app.listen (process.env.port, () => {
-          console.log ("we are online")
+app.listen(process.env.port, () => {
+    console.log("we are online")
 })
